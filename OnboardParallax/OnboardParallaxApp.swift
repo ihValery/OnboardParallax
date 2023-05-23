8e345e9bbf9253e886f68bea642fc1bfc00db729
ihValery <ihValery@gmail.com>
2023-05-23 03:40:59 +0300

    Initial Commit

---
+++ OnboardParallax/OnboardParallaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardParallaxApp.swift
+//  OnboardParallax
+//
+//  Created by Валерий Игнатьев on 23.05.23.
+//
+
+import SwiftUI
+
+@main
+struct OnboardParallaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
